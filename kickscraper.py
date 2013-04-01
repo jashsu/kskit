1d4449aee9326a3581e53368f85a98aec1018485
Jason Hsu <jashsu@gmail.com>
2013-04-01 04:53:56 -0700

    enhancements all around

--- kickscraper.py
+++ kickscraper.py
@@ -1,25 +1,116 @@
-'''
-Kickstarter scraper to help mine info about a project's backers
-Usage:
-python ./kickscraper.py http://www.kickstarter.com/projects/2093540459/msj-the-musical-0 2
-'''
+#!/usr/bin/env python
+""" kickscraper.py: A simple Kickstarter project and user data scraping tool.
+    Usage: python ./kickscraper.py <project url> <backer threshold> <category threshold>
+        backer threshold: minimum integer number of backers to display project in common
+        category threshold: minimum decimal percentage to display category"""
 
 import requests, time, json, sys, gzip, operator
 from sys import stdout
 from lxml import etree
 from StringIO import StringIO
 from urllib import urlencode
+from urlparse import urlparse
 from random import random
 from HTMLParser import HTMLParser
 
-def scrape_project_backers(creator, project, sleep=.5, spread=.5):
+__author__ = 'Jason Hsu'
+__version__ = '1'
+
+class DeletedUserError(Exception):
+    pass
+
+class ProgressBar(object):
+    def __init__(self):
+        self.counter = 0
+
+    def increment(self, n=1):
+        self.counter += n
+
+    def draw(self, tick='.'):
+        draw_out = ''
+        if self.counter % 50 == 0:  draw_out += '\n[{}] '.format(time.ctime())
+        if self.counter % 10 == 0:  draw_out += tick # For optional fancier gfx
+        else:                       draw_out += tick
+        stdout.write(draw_out)
+        stdout.flush()
+
+class ProjectCacheV1(object):
+    """Simple object for caching project information. Todo: upgrade to db."""
+
+    __version__ = 1
+    project_cache_path = './project_cache/v1.json.gz'
+
+    def initialize(self):
+        """Initialize a new empty project cache."""
+
+        f = gzip.open(self.project_cache_path, 'w+')
+        f.write(json.dumps({}))
+        f.close()
+
+    def refresh(self):
+        """Scan through the project cache and refresh all project information."""
+
+        # todo: implement this
+        pass
+
+    def update(self, creator, project, force=False):
+        """Adds a project to the project cache if it hasn't already been cached."""
+
+        sanitizer = [('\\\\"', "'"),]
+
+        while True:
+            try:
+                f = gzip.open(self.project_cache_path, 'rb')
+                project_cache = json.loads(f.read())
+            except:
+                stdout.write('\nUnable to read project cache, attempting to regenerate')
+                self.initialize()
+                continue
+            break
+
+        f.close()
+        if project in project_cache and force == False:
+            return  # Exit quickly if we can
+
+        f = gzip.open(self.project_cache_path, 'wb')
+        project_url = 'http://www.kickstarter.com/projects/{}/{}'.format(creator, project)
+        session = requests.Session()
+        try:
+            response = session.get(project_url)
+            response_tree = etree.parse(StringIO(response.text), etree.HTMLParser(encoding = 'UTF-8'))
+            current_project = HTMLParser().unescape(response_tree.xpath('//head/script[contains(text(), "window.current_project")]')[0].text.split('"')[1])
+            for rule in sanitizer:
+                current_project = current_project.replace(*rule)
+            project_cache[project] = json.loads(current_project)
+        finally:
+            f.write(json.dumps(project_cache))
+            session.close()
+            f.close()
+
+    def query(self, project):
+        """Get information about a project from the project cache."""
+
+        f = gzip.open(self.project_cache_path, 'rb')
+        project_cache = json.loads(f.read())
+        f.close()
+        return project_cache[project]    # Should be present
+
+class UserCacheV1(object):
+    """Simple object for caching user information. Todo: implement this."""
+
+    pass
+
+def scrape_project_backers(creator, project, sleep=.2, spread=.1):
+    """Scrape the list of backers from the target project."""
+
+    progress_bar = ProgressBar()
     session = requests.Session()
     params = {}
     last_page = False
     project_backers = []
     project_backers_url = 'http://www.kickstarter.com/projects/{}/{}/backers'.format(creator, project)
 
-    stdout.write('\nScraping project backer list\n')
+    stdout.write('\n[{}] Scraping project backer list'.format(time.ctime()))
     while(not last_page):
         response = session.get('{}?{}'.format(project_backers_url, urlencode(params)))
         response_tree = etree.parse(StringIO(response.text), etree.HTMLParser(encoding = 'UTF-8'))
@@ -27,14 +118,17 @@ def scrape_project_backers(creator, project, sleep=.5, spread=.5):
         last_page = {'false': False, 'true': True}[page.attrib['data-last_page']]
         project_backers.extend(page.getchildren())
         params['cursor'] = project_backers[-1].attrib['data-cursor']
-        stdout.write('.')
-        stdout.flush()
-        if not last_page: time.sleep(sleep + random() * spread)
+        progress_bar.draw()
+        progress_bar.increment()
+        if not last_page:
+            time.sleep(sleep + random() * spread)
 
     session.close()
     return project_backers
 
-def scrape_user_info(user_slug, session, sleep=.5, spread=.5):
+def scrape_user_info(user_slug, session, sleep=.2, spread=.1):
+    """Scrape information about the user, including the list of backed projects."""
+
     params = {}
     last_page = False
     user_profile_url = 'http://www.kickstarter.com/profile/{}'.format(user_slug)
@@ -46,12 +140,15 @@ def scrape_user_info(user_slug, session, sleep=.5, spread=.5):
     while(not last_page):
         response = session.get('{}?{}'.format(user_profile_url, urlencode(params)))
         response_tree = etree.parse(StringIO(response.text), etree.HTMLParser(encoding = 'UTF-8'))
+        if 'This person is no longer active on Kickstarter.' in response.text:
+            raise DeletedUserError
         page = response_tree.xpath("//li[@class='page']")[0]
         last_page = {'false': False, 'true': True}[page.attrib['data-last_page']]
         backed_projects = [tuple(project_url.split('/')[-2:]) for project_url in page.xpath('./a/@href')]
         user_info['user_backed_projects'].extend(backed_projects)
         params['page'] = int(page.attrib['data-page_number']) + 1
-        if not last_page: time.sleep(sleep + random() * spread)
+        if not last_page:
+            time.sleep(sleep + random() * spread)
 
     wishlist = [('user_name', "//meta[@property='kickstarter:name']/@content"),
                 ('user_joined', "//meta[@property='kickstarter:joined']/@content"),
@@ -61,94 +158,97 @@ def scrape_user_info(user_slug, session, sleep=.5, spread=.5):
 
     return user_info
 
-def process_project_backers(project_backers):
+def process_project_backers(creator, project, project_backers):
+    """Process the list of backers into dicts and scrape the user details."""
+
+    progress_bar = ProgressBar()
     session = requests.Session()
     project_backers_processed = []
+    project_backers_path = './project_backers/{}_{}_{}.json'
 
-    stdout.write('\nScraping users backed projects lists\n')
+    stdout.write('\n[{}] Scraping users backed projects lists'.format(time.ctime()))
     for user in project_backers:
         user_slug = user.xpath("./a/@href")[0].split('/')[2]
-        project_backers_processed.append(scrape_user_info(user_slug, session))
-        stdout.write('.')
-        stdout.flush()
+        try:
+            user_info = scrape_user_info(user_slug, session)
+        except DeletedUserError:
+            progress_bar.draw('x')
+        else:
+            project_backers_processed.append(user_info)
+            progress_bar.draw()
+        progress_bar.increment()
 
     session.close()
-    return project_backers_processed
-
-def initialize_project_cache_v1():
-    f = gzip.open('project_cache_v1.json.gz', 'wb')
-    f.write(json.dumps({}))
-    f.close()
-
-def update_project_cache_v1(creator, project, force=False):
-    sanitizer = [('\\\\"', "'"),]
-
-    while True:
-        try:
-            f = gzip.open('project_cache_v1.json.gz', 'rb')
-            project_cache_v1 = json.loads(f.read())
-        except:
-            stdout.write('\nUnable to read project cache; attempting to regenerate')
-            initialize_project_cache_v1()
-            continue
-        break
-
-    f.close()
-    if project in project_cache_v1 and force == False:
-        return
-
-    f = gzip.open('project_cache_v1.json.gz', 'wb')
-    stdout.write('\nUpdating project_cache: {}'.format(project))
-    project_url = 'http://www.kickstarter.com/projects/{}/{}'.format(creator, project)
-    session = requests.Session()
-    try:
-        response = session.get(project_url)
-        response_tree = etree.parse(StringIO(response.text), etree.HTMLParser(encoding = 'UTF-8'))
-        current_project = HTMLParser().unescape(response_tree.xpath('//head/script[contains(text(), "window.current_project")]')[0].text.split('"')[1])
-        for rule in sanitizer:
-            current_project = current_project.replace(*rule)
-        project_cache_v1[project] = json.loads(current_project)
-    finally:
-        f.write(json.dumps(project_cache_v1))
-        session.close()
-        f.close()
-
-def query_project_cache_v1(project):
-    f = gzip.open('project_cache_v1.json.gz', 'rb')
-    project_cache_v1 = json.loads(f.read())
-    f.close()
-    return project_cache_v1[project]
-
-def print_similarity_statistics(project_backers_processed, project_threshold):
-    count = {}
-
-    for user in project_backers_processed:
-        for creator_project in user['user_backed_projects']:
-            update_project_cache_v1(creator_project[0], creator_project[1])
-            if creator_project[1] in count:
-                count[creator_project[1]] += 1
-            else:
-                count[creator_project[1]] = 1
-
-    sorted_count = sorted(count.iteritems(), key=operator.itemgetter(1), reverse=True)
-    stdout.write('\n')
-    for project in sorted_count:
-        if project[1] >= int(project_threshold):
-            current_project = query_project_cache_v1(project[0])
-            stdout.write('\n{}\n    {} backers in common\n    {} ({})'.format(project[0], project[1], current_project['category']['name'], current_project['category']['id']))
-
-def main(args):
-    project_url, project_threshold = args[1:]
-    creator, project = project_url.split('/')[4:6]
-
-    project_backers = scrape_project_backers(creator, project)
-    project_backers_processed = process_project_backers(project_backers)
-    f = open('{}_{}.json'.format(project, int(time.time())), 'wb')
+    f = open(project_backers_path.format(creator, project, int(time.time())), 'w+')
     f.write(json.dumps(project_backers_processed))
     f.close()
-    print_similarity_statistics(project_backers_processed, project_threshold)
 
-    raw_input('\n\nPress any key to exit...\n')
+    return project_backers_processed
+
+def print_similarity_statistics(project_cache, project_backers_processed, project_threshold, category_threshold):
+    """Print statistical information about what other projects backers have backed."""
+
+    progress_bar = ProgressBar()
+    projects_data = {}
+    category_data = {}
+
+    stdout.write('\n[{}] Compiling project similarity statistics'.format(time.ctime()))
+    for user in project_backers_processed:
+        progress_bar.draw()
+        progress_bar.increment()
+        for creator_project in user['user_backed_projects']:
+            project_cache.update(*creator_project)
+            project = creator_project[1]
+            if project in projects_data:
+                projects_data[project][0] += 1   # Increment count
+            else:
+                projects_data[project] = [1, 0, '']
+
+    stdout.write('\n[{}] Reticulating splines'.format(time.ctime()))
+    stdout.flush()
+    for project in projects_data:
+        current_project = project_cache.query(project)
+        projects_data[project][1] = category_id = current_project['category']['id']
+        projects_data[project][2] = category_name = current_project['category']['name']
+        # projects_data := {project: [project_count, category_id, category_name], ...}
+        # current_project := Kickstarter project API json object (window.current_project)
+        if category_id in category_data:
+            category_data[category_id][0] += projects_data[project][0]
+        else:
+            category_data[category_id] = [projects_data[project][0], category_name]
+        # category_data := {category_id: [category_count, category_name]}
+
+    sorted_projects_data = sorted(projects_data.iteritems(), key=lambda (k,v): v[0], reverse=True)
+    # sorted_projects_data := [(project, [project_count, category_id, category_name]), ...]
+
+    stdout.write('\n[{0}]\n[{0}] Projects in common (popularity sort)'.format(time.ctime()))
+    for project_data in sorted_projects_data:
+        if project_data[1][0] >= int(project_threshold):
+            stdout.write('\n[{}] {}'.format(time.ctime(), project_data[0]))
+            stdout.write('\n[{}]     {} common backers'.format(time.ctime(), project_data[1][0]))
+            stdout.write('\n[{}]     {} ({})'.format(time.ctime(), project_data[1][2], project_data[1][1]))
+            stdout.flush()
+
+    category_sum = sum([i[0] for i in category_data.values()])
+    sorted_category_data = sorted(category_data.iteritems(), key=lambda (k,v): v[0], reverse=True)
+    # sorted_category_data := [(category_id, [category_count, category_name]), ...]
+
+    stdout.write('\n[{0}]\n[{0}] Favorite categories (popularity sort)'.format(time.ctime()))
+    for category_data in sorted_category_data:
+        category_match = float(category_data[1][0])/category_sum
+        if category_match >= float(category_threshold):
+            stdout.write('\n[{}] {}% {} ({})'.format(time.ctime(), round(category_match*100, 1), category_data[1][1], category_data[0]))
+
+def main(args):
+    project_url, project_threshold, category_threshold = args[1:]
+    creator, project = urlparse(project_url)[2].split('/')[2:4]
+    project_cache = ProjectCacheV1()
+
+    project_backers = scrape_project_backers(creator, project)
+    project_backers_processed = process_project_backers(creator, project, project_backers)
+    print_similarity_statistics(project_cache, project_backers_processed, project_threshold, category_threshold)
+
+    stdout.write('\n\n')
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
